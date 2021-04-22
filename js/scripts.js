c913b72ad5a24cb70f9dc77ab40ca80a1d4d9e6b
Axel Ingouf <axel.ingouf@gmail.com>
2021-04-21 23:41:31 -0400

    Improved scripting and added download links

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,22 @@
 var clickedItem = false;
 var selectedItem = -1;
 
+$('.description-content').on('click', '.quit-button', function() {
+  $('.selected').fadeOut();
+  $('.selected').removeClass("selected");
+  deleteQuitButton();
+  clickedItem = false;
+  selectedItem = -1;
+});
+
+
 $(document).on('click', window, function() {
 
   if(clickedItem == false)
   {
-    $('.selected').css("display", "none");
+    $('.selected').fadeOut();
     $('.selected').removeClass("selected");
+    deleteQuitButton();
     selectedItem = -1;
   }
   else
@@ -18,17 +28,21 @@ $(document).on('click', window, function() {
 
 $('.item').on('click', function()
 {
-  $('.selected').css("display", "none");
-  $('.selected').removeClass("selected");
 
   var i = ($(this).index() - 1) / 2;
 
   if(i != selectedItem)
   {
+    $('.selected').css("display", "none");
+    $('.selected').removeClass("selected");
+    deleteQuitButton();
+
     var description = $('.project-description').eq(i);
 
-    description.css("display", "block");
+    description.fadeIn();
+    description.css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 'slow');
     description.addClass("selected");
+    addQuitButton();
   
     clickedItem = true;
 
@@ -36,11 +50,28 @@ $('.item').on('click', function()
   }
   else
   {
+    $('.selected').fadeOut();
+    $('.selected').removeClass("selected");
+    deleteQuitButton();
+
     selectedItem = -1;
     clickedItem = false;
   }
 });
 
+function addQuitButton()
+{
+  $('.selected').children('.description-content').prepend("<div class=\"quit-button\"></div>");
+}
+
+function deleteQuitButton()
+{
+  $('.quit-button').animate({visibility: 0}, 200,"linear",function()
+  {
+      $(this).remove();
+  }
+)
+}
 
 $('.description-content').on('click', function()
 {
